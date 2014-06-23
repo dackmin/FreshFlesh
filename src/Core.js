c027f23014849fddb899ee77fc6d405aa188283d
Ugo Stephant <contact@dackmin.com>
2014-06-23 15:17:18 +0200

    Adding core file

---
+++ src/Core.js
@@ -0,0 +1,40 @@
+/**
+ * FreshFlesh
+ * @version 0.1 (June 2014)
+ * @author Ugo Stephant (http://www.ugostephant.io/)
+ *
+ * FreshFlesh is licensed under the Apache V2 License.
+ * http://opensource.org/licenses/Apache-2.0
+ */
+var FF = {};
+
+
+/**
+ * Array to store preloaded images
+ * @property TextureCache
+ * @type Array<Image>
+ */
+FF.TextureCache = [];
+
+
+/**
+ * Array to store preloaded tmx maps
+ * @property TMXMapCache
+ * @type Array<TMXMap>
+ */
+FF.TMXMapCache = [];
+
+
+/**
+ * Array to store bitmap font files
+ * @property BitmapTextCache
+ *
+ */
+FF.BitmapFontCache = [];
+
+
+/**
+ * Array to store sounds
+ * @property AudioCache
+ */
+FF.AudioCache = [];
